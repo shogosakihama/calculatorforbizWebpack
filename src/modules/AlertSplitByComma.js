4ea2cc9156495e48ed31f47bf5eb028ce3ebccc2
Shogo <goshalways@gmail.com>
2022-08-11 20:56:05 +0900

    RetSource整理した

--- src/modules/AlertSplitByComma.js
+++ src/modules/AlertSplitByComma.js
@@ -2,51 +2,6 @@ import RetSource from './RetSource.js';
 window.RetSource = RetSource;
 export default class AlertSplitByComma {
     constructor() {
-        
-        // ▼出力用の変数
-        var RetSource1 = [];
-        // ▼テキスト入力欄に入力された文字列を得て、カンマ記号で分割して配列に得る
-        var resArray1 = document.getElementById('sampleCommaInput1').value.split(",");
-        if (resArray1 == "") {
-            resArray1 = [];
-        };
-
-        // ▼配列1つ1つに対して処理
-        for (var i = 0; i < resArray1.length; i++) {
-            RetSource1.push(resArray1[i]);
-        }
-
-        
-
-
-        // ▼出力用の変数
-        var RetSource2 = [];
-        // ▼テキスト入力欄に入力された文字列を得て、カンマ記号で分割して配列に得る
-        var resArray2 = document.getElementById('sampleCommaInput2').value.split(",");
-        if (resArray2 == "") {
-            resArray2 = [];
-        };
-
-        // ▼配列1つ1つに対して処理
-        for (var i = 0; i < resArray2.length; i++) {
-            RetSource2.push(resArray2[i]);
-            console.log(RetSource2);
-        }
-
-
-        // ▼出力用の変数
-        var RetSource3 = [];
-        // ▼テキスト入力欄に入力された文字列を得て、カンマ記号で分割して配列に得る
-        var resArray3 = document.getElementById('sampleCommaInput3').value.split(",");
-        if (resArray3 == "") {
-            resArray3 = [];
-        };
-
-        // ▼配列1つ1つに対して処理
-        for (var i = 0; i < resArray3.length; i++) {
-            RetSource3.push(resArray3[i]);
-            console.log(RetSource3);
-        }
 
         let input1 = 'sampleCommaInput1';
         let input2 = 'sampleCommaInput2';
@@ -71,9 +26,6 @@ export default class AlertSplitByComma {
             return self.indexOf(x) === i;
         });
 
-        //配列を1つにまとめる
-        //var arrs = [arr1, arr2, arr3];
-
         var arrsCon = arr1.concat(arr2).concat(arr3);
 
 
@@ -96,10 +48,7 @@ export default class AlertSplitByComma {
             // 最初に見つかった位置と最後に見つかった位置が一緒であれば
             return arr1.indexOf(val) === arrx.indexOf(val);
         });
-        console.log("arr1: " + arr1a);
-        if (RetSource1 == ["e", "e"]) {
-            console.log("一致");
-        }
+
 
         let arr2a = arrx.filter(function (val, idx, arr2) {
             // 最初に見つかった位置と最後に見つかった位置が一緒であれば
